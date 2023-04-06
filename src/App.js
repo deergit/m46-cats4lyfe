c3758acb524283940ebe6f0f5b827be572575708
deergod <CommonTroll@live.co.uk>
2023-04-06 11:40:40 +0100

    restructuring data to be more usable

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import { useState, useEffect, useRef } from 'react';
-import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { faker } from '@faker-js/faker';
 import BasketModal from './components/BasketModal';
 import Navbar from './Navbar';
+import './App.css';
 
 // notes: to use more than 10 images you need the API key
 
 const App = () => {
-
     const APIKey = 'live_lpMrF7D80GK4J5PqOfjBJevGbqZk56CYvauSH2nzay440sP7RN7ILmCIL5yViVyy';
-    const [images, setImages] = useState([]);
+    const [catData, setCatData] = useState([]);
     const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
-        const cachedImages = JSON.parse(localStorage.getItem('images'));
+        const cachedData = JSON.parse(localStorage.getItem('catCacheData'));
 
         // images stored in cache to stop fetching in a loop
-        if (cachedImages) {
-            setImages(cachedImages.slice(0, 9));               
+        if (cachedData && cachedData.length > 0) {
+            setCatData(cachedData);
         } else {
-            fetch('https://api.thecatapi.com/v1/images/search?limit=12?api_key=live_lpMrF7D80GK4J5PqOfjBJevGbqZk56CYvauSH2nzay440sP7RN7ILmCIL5yViVyy')
-                .then(response => response.json())
-                .then(data => {
-                    setImages(data.slice(0, 9));                            // selecting 9 images
-                    localStorage.setItem('images', JSON.stringify(data));
-                });
-        }
-    }, []);
+            const fetchCatdata = async () => {
+                const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=12`);
+                const data = await response.json();
+                const catConstructor = data.map((cat, index) => {
+                    return {
+                        id: index,
+                        url: cat.url,
+                        name: faker.name.fullName(),
+                        breed: faker.animal.cat(),
+                        description: faker.word.adjective(),
+                        price: faker.commerce.price(50, 2500, 2, "£")
+                    }
+                })
+                setCatData(catConstructor);
+                localStorage.setItem('catCacheData', JSON.stringify(catConstructor));
+                console.log(catConstructor);
+                };
+            fetchCatdata();
+            }
+        }, []);
 
     const handleAddToBasket = () => {
         // Add logic for adding product to basket
@@ -43,25 +55,27 @@ const App = () => {
         <div className="container">
             <Navbar />
             <div className="image-grid">
-                {images.map((image, index) => (
-                    <div className="grid-item" key={index}>
-                        <img src={image.url} alt={`Cat ${index}`} />
-                        {image.breeds ? (
-                            <div className="overlay">
-                                <p>{image.breeds[0].name}</p>
-                                <p>{image.breeds[0].description}</p>
-                                <button onClick={handleAddToBasket}>Add to Basket</button>
-                                {showModal && (
-                                    <BasketModal
-                                        onClose={handleCloseModal}
-                                        onAddToBasket={() => {
-                                            handleCloseModal();
-                                        }}
-                                    />)}
-                            </div>)
-                        : null}
-                    </div>
-                ))}
+                {catData.map((cat) => {
+                    return (
+                        <div className="grid-item" key={cat.id}>
+                            <img src={catData.url} alt={`Cat ${cat.id}`} />
+                            {catData.breed ? (
+                                <div className="overlay">
+                                    <p>{catData.breed}</p>
+                                    <p>{catData.breed}</p>
+                                    <button onClick={handleAddToBasket}>Add to Basket</button>
+                                    {showModal && (
+                                        <BasketModal
+                                            onClose={handleCloseModal}
+                                            onAddToBasket={() => {
+                                                handleCloseModal();
+                                            }}
+                                        />)}
+                                </div>)
+                            : null}
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
