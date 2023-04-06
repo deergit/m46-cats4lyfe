18101382ac956bccf3779f27fdbd708c5ffac19e
deergod <CommonTroll@live.co.uk>
2023-04-06 12:00:21 +0100

    removing test

--- src/App.js
+++ src/App.js
@@ -29,8 +29,7 @@ const App = () => {
                         name: faker.name.fullName(),
                         breed: faker.animal.cat(),
                         description: faker.word.adjective(),
-                        price: faker.commerce.price(50, 2500, 2, "£"),
-                        test: "testing"
+                        price: faker.commerce.price(50, 2500, 2, "£")
                     }
                 })
                 setCatData(catConstructor);
