e84df6aa69711c6ed3226a80bfb6335badf60913
deergod <CommonTroll@live.co.uk>
2023-04-06 14:23:01 +0100

    adding logo to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,10 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="navbarLogo">My Store</h1>
+      <div className="navLogo">
+        <img className="navLogoImg" src="./images/cat.png" alt="cats 4 lyfe logo"></img>
+        <h1 className="navLogoText">Cats 4 Lyfe</h1>
+      </div>
       <div className="navbarLinks">
         <button className="basketButton">Basket</button>
       </div>
